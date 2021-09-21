9ef3ccd4cb6d4bd4662afff903b14b1bbfff217d
taqyrrahman <taqyrrahman@gmail.com>
2021-09-21 21:12:51 +0700

    first commit

---
+++ installer/valheim_installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo echo steam steam/license note '' | sudo debconf-set-selections
+sudo echo steam steam/question select 'I AGREE' | sudo debconf-set-selections
+#'
+sudo apt install lib32gcc1 steamcmd -y
+exit
+mkdir /home/qittu/valheim
+steamcmd +login anonymous +force_install_dir /home/qittu/valheim +app_update 896660 validate +exit
